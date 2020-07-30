6795e8f5c97ba2f10d05725faf4999cfba785fdd
Donald Morton <donald.morton@graylog.com>
2020-07-30 11:06:52 -0500

    Fix test in Ubuntu 20.04.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,14 +2,18 @@ import os
 
 import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_service_elasticsearch_running(host):
     assert host.service("elasticsearch").is_running is True
 
 def test_service_mongodb_running(host):
-    assert host.service("mongod").is_running is True
+    if host.system_info.distribution == 'ubuntu' and host.system_info.codename == 'focal':
+        mongodb_service_name = 'mongodb'
+    else:
+        mongodb_service_name = 'mongod'
+
+    assert host.service(mongodb_service_name).is_running is True
 
 def test_is_graylog_installed(host):
     assert host.package('graylog-server').is_installed
