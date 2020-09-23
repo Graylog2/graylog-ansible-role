a673bc6b3b9daf27404e4d330819bebc25a73608
Donald Morton <donald.morton@graylog.com>
2020-09-23 15:58:54 -0500

    Add Selenium test for basic logic.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,8 +1,36 @@
 import os
 
 import testinfra.utils.ansible_runner
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_basic_login(host):
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
+
+    url = 'http://' + host.interface('eth0').addresses[0] + ':9000'
+
+    driver.get(url + "/gettingstarted")
+
+    element = wait.until(EC.title_is(('Graylog - Sign in')))
+
+    #login
+    uid_field = driver.find_element_by_name("username")
+    uid_field.clear()
+    uid_field.send_keys("admin")
+    password_field = driver.find_element_by_name("password")
+    password_field.clear()
+    password_field.send_keys("admin")
+    password_field.send_keys(Keys.RETURN)
+
+    element = wait.until(EC.title_is(('Graylog - Getting started')))
+
+    driver.close()
 
 def test_service_elasticsearch_running(host):
     assert host.service("elasticsearch").is_running is True
