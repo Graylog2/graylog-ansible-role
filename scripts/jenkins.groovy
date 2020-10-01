26e07f50d08c7728528cffa830de4162ac6e9d9f
Donald Morton <donald.morton@graylog.com>
2020-10-01 10:53:41 -0500

    Fix syntax error. Add stage.

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -1,33 +1,21 @@
-  pipeline
-  {
-      agent none
+pipeline
+{
+    agent none
 
-     options
-     {
-        ansiColor('xterm')
-        buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '10', daysToKeepStr: '30', numToKeepStr: '10')
-        timestamps()
-     }
+   options
+   {
+      ansiColor('xterm')
+      buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '10', daysToKeepStr: '30', numToKeepStr: '10')
+      timestamps()
+   }
 
-     stages
-     {
-        stage('Install and Validate')
+   stages
+   {
+      stage('Install and Validate')
+      {
+        parallel
         {
-          parallel
-          {
-            stage('Ubuntu 20.04')
-            {
-              agent
-              {
-                label 'linux'
-              }
-              steps
-              {
-                sh 'echo test'
-              }
-            }
-          }
-          stage('Ubuntu 18.04')
+          stage('Ubuntu 20.04')
           {
             agent
             {
@@ -39,6 +27,17 @@
             }
           }
         }
-     }
-  }
+        stage('Ubuntu 18.04')
+        {
+          agent
+          {
+            label 'linux'
+          }
+          steps
+          {
+            sh 'echo test'
+          }
+        }
+      }
+   }
 }
