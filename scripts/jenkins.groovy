1b07de621315ed429f69240aa37142646150e952
Donald Morton <donald.morton@graylog.com>
2020-10-02 14:56:16 -0500

    Add more OS's and stages.

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -23,7 +23,7 @@ pipeline
             }
             steps
             {
-              sh 'molecule test'
+              sh 'MOLECULE_DISTRO='generic/ubuntu2004'; molecule test'
             }
           }
           stage('Ubuntu 18.04')
@@ -34,7 +34,84 @@ pipeline
             }
             steps
             {
-              sh 'molecule test'
+              sh 'MOLECULE_DISTRO='generic/ubuntu1804'; molecule test'
+            }
+          }
+          stage('Ubuntu 16.04')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'MOLECULE_DISTRO='generic/ubuntu1604'; molecule test'
+            }
+          }
+          stage('Debian Buster')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'export MOLECULE_DISTRO='debian/buster64'; molecule test'
+            }
+          }
+          stage('Debian Stretch')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'export MOLECULE_DISTRO='debian/stretch64'; molecule test'
+            }
+          }
+          stage('Debian Jessie')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'export MOLECULE_DISTRO='debian/stretch64'; molecule test'
+            }
+          }
+          stage('Debian Jessie')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'export MOLECULE_DISTRO='debian/jessie64'; molecule test'
+            }
+          }
+          stage('CentOS 8')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'export MOLECULE_DISTRO='centos/8'; molecule test'
+            }
+          }
+          stage('CentOS 7')
+          {
+            agent
+            {
+              label 'linux'
+            }
+            steps
+            {
+              sh 'export MOLECULE_DISTRO='centos/7'; molecule test'
             }
           }
         }
