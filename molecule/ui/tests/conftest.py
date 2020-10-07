33f1ca7051a38e71f4379e3be2eb06632ed50320
Donald Morton <donald.morton@graylog.com>
2020-10-07 13:58:04 -0500

    Wait for node dropdown to load before clicking it. Ensure chromedriver properly exits after tests.

--- molecule/ui/tests/conftest.py
+++ molecule/ui/tests/conftest.py
@@ -3,39 +3,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 import time
 
 @pytest.fixture(scope="session")
 def chromedriver():
-    options = Options()
-    options.headless = True
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--disable-gpu");
+    try:
+        options = Options()
+        options.headless = True
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--disable-gpu");
 
-    driver = webdriver.Chrome(options=options)
-    wait = WebDriverWait(driver, 10)
+        driver = webdriver.Chrome(options=options)
+        wait = WebDriverWait(driver, 10)
 
-    time.sleep(60) #Wait for Graylog to finish starting up
+        time.sleep(60) #Wait for Graylog to finish starting up
 
-    url = 'http://localhost:9000'
-    driver.get(url + "/gettingstarted")
+        url = 'http://localhost:9000'
+        driver.get(url + "/gettingstarted")
 
-    element = wait.until(EC.title_is('Graylog - Sign in'))
+        element = wait.until(expected_conditions.title_is('Graylog - Sign in'))
 
-    #Login to Graylog
-    uid_field = driver.find_element_by_name("username")
-    uid_field.clear()
-    uid_field.send_keys("admin")
-    password_field = driver.find_element_by_name("password")
-    password_field.clear()
-    password_field.send_keys("admin")
-    password_field.send_keys(Keys.RETURN)
+        #Login to Graylog
+        uid_field = driver.find_element_by_name("username")
+        uid_field.clear()
+        uid_field.send_keys("admin")
+        password_field = driver.find_element_by_name("password")
+        password_field.clear()
+        password_field.send_keys("admin")
+        password_field.send_keys(Keys.RETURN)
 
-    element = wait.until(EC.title_is('Graylog - Getting started'))
+        element = wait.until(expected_conditions.title_is('Graylog - Getting started'))
 
-    #Run tests
-    yield driver
-
-    driver.close()
+        #Run tests
+        yield driver
+    finally:
+        driver.close()
+        driver.quit()
--- molecule/ui/tests/test_ui.py
+++ molecule/ui/tests/test_ui.py
@@ -2,7 +2,8 @@ import pytest
 import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 
 class TestGraylog():
     url = 'http://localhost:9000'
@@ -20,8 +21,9 @@ class TestGraylog():
     def test_input_udp(self, chromedriver):
         print('Testing GELF UDP Input...')
         chromedriver.get(self.url + "/system/inputs")
+
         wait = WebDriverWait(chromedriver, 10)
-        element = wait.until(EC.title_is('Graylog - Inputs'))
+        element = wait.until(expected_conditions.title_is('Graylog - Inputs'))
 
         #Click input dropdown
         chromedriver.find_element_by_css_selector(".form-group").click()
@@ -32,8 +34,11 @@ class TestGraylog():
         #Click "Launch new Input" button
         chromedriver.find_element_by_xpath('//button[text()="Launch new input"]').click()
 
+        #Wait for node dropdown to load
+        WebDriverWait(chromedriver, 5).until(expected_conditions.presence_of_element_located((By.ID, 'node-select')))
+
         #Select node
-        chromedriver.find_element_by_xpath("//select[@name='node-select']/option[2]").click()
+        chromedriver.find_element_by_xpath("//select[@id='node-select']/option[2]").click()
 
         #Fill out input field
         title_field = chromedriver.find_element_by_id("title")
