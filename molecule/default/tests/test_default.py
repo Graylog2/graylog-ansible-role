1f7b3b9a978edfe85a6fbfb13f6e0f1b2d7f99bc
Frank Felhoffer <frank.felhoffer@securekey.com>
2021-03-31 22:26:51 -0400

    Fix failing mongodb test on ubuntu 20.04

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,12 +3,7 @@ def test_service_elasticsearch_running(host):
     assert host.service("elasticsearch").is_running is True
 
 def test_service_mongodb_running(host):
-    if host.system_info.distribution == 'ubuntu' and host.system_info.codename == 'focal':
-        mongodb_service_name = 'mongodb'
-    else:
-        mongodb_service_name = 'mongod'
-
-    assert host.service(mongodb_service_name).is_running is True
+    assert host.service("mongod").is_running is True
 
 def test_is_graylog_installed(host):
     assert host.package('graylog-server').is_installed
