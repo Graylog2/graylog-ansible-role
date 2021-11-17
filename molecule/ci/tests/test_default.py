ec669e257eefc8592cf8c9a74750691df58d77e6
Donald Morton <donald.morton@graylog.com>
2021-11-17 08:58:28 -0600

    Create new scenario just for CI.

---
+++ molecule/ci/tests/test_default.py
@@ -0,0 +1,10 @@
+"""Role testing files using testinfra."""
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
